4c8ddcfef0f86020174df10c930e66aea3a56b6b
DaniilShmoylov <daniil.shmoylove@gmail.com>
2023-01-16 01:51:37 +0300

    Initial Commit

---
+++ CounterML-iOS/CounterML_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterML_iOSApp.swift
+//  CounterML-iOS
+//
+//  Created by Daniil Shmoylove on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterML_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
