9ca7cde2a18a84fa8e178d249241fc08e5218d31
DaniilShmoylov <daniil.shmoylove@gmail.com>
2023-02-12 16:47:33 +0300

    Create two CoreUI animation modifier
    - PulseModifier (Pulse animation view modifier)
    
    - RotationEffectModifier (Rotation effect animation view modifier)

---
+++ CounterMLKit/Sources/CoreUI/Animations/RotationEffect.swift
@@ -0,0 +1,43 @@
+//
+//  RotationEffect.swift
+//  
+//
+//  Created by Daniil Shmoylove on 23.01.2023.
+//
+
+import SwiftUI
+
+//MARK: - View modifier
+
+public extension View {
+    func rotatableAnimation() -> some View {
+        self.modifier(RotationEffectModifier())
+    }
+}
+
+//MARK: - Rotation effect modifier
+
+/* Rotation effect animation view modifier */
+
+struct RotationEffectModifier: ViewModifier {
+    
+    @State private var isRotationEffect: Bool = false
+    
+    func body(content: Content) -> some View {
+        content
+            .rotation3DEffect(
+                .degrees(self.isRotationEffect ? 12: -12),
+                axis: (
+                    x: self.isRotationEffect ? 90 : -45,
+                    y: self.isRotationEffect ? -45 : -90, z: 0
+                )
+            )
+            .animation(
+                .easeInOut(duration: 6).repeatForever(),
+                value: self.isRotationEffect
+            )
+            .onAppear() {
+                self.isRotationEffect = true 
+            }
+    }
+}
--- CounterMLKit/Sources/Resources/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Daniil Shmoylove on 18.01.2023.
-//
-
-import Foundation
