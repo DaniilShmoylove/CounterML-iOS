aa18c0cb799e7db0794acaad59f73348e61d5519
DaniilShmoylov <daniil.shmoylove@gmail.com>
2023-02-17 22:31:39 +0300

    Add Lifecycle files for iOS and macOS (UIApplicationDelegate, NSApplicationDelegate)

---
+++ CounterML-iOS/Lifecycle/NSAppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  NSAppDelegate.swift
+//  CounterML-iOS
+//
+//  Created by Daniil Shmoylove on 21.01.2023.
+//
+
+#if canImport(AppKit)
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(
+        _ notification: Notification
+    ) { }
+}
+#endif
---
+++ CounterML-iOS/Lifecycle/UIAppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  CounterML-iOS
+//
+//  Created by Daniil Shmoylove on 21.01.2023.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool { return true }
+}
+#endif
