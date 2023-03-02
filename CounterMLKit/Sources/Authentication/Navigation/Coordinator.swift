88c1d536ed67706d5eeba7c097c8c0d5e366657a
DaniilShmoylov <daniil.shmoylove@gmail.com>
2023-03-02 13:03:16 +0300

    Create Coordinator & Router for Auth navigation.

---
+++ CounterMLKit/Sources/Authentication/Navigation/Coordinator.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinator.swift
+//  
+//
+//  Created by Daniil Shmoylove on 02.03.2023.
+//
+
+import Foundation
+
+//MARK: - Coordinator
+
+final class Coordinator: ObservableObject {
+    @Published var path = [Route]()
+}
