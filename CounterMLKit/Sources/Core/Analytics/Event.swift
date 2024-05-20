af83bce49470aa12e41a6ae2473203e9940ec686
Daniil Shmoylov <daniil.shmoylove@gmail.com>
2024-05-20 12:03:38 +0300

    Added analytics events for authentication methods

--- CounterMLKit/Sources/Core/Analytics/Event.swift
+++ CounterMLKit/Sources/Core/Analytics/Event.swift
@@ -7,4 +7,11 @@
 
 public enum Event: String {
     case LAUNCH_APP
+    
+    //MARK: - Authentication
+    
+    case AUTHENTICATION_SIGN_IN_WITH_GOOGLE
+    case AUTHENTICATION_SIGN_IN
+    case AUTHENTICATION_SIGN_UP
+    case AUTHENTICATION_SIGN_OUT
 }
